cccbb5c25da1e74bfbdfe1b1f06ed093716c90b0
Fiona-alice <alicce.fiona@gmail.com>
2023-06-20 11:40:06 +0300

    glory

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+     <ol id="relativeList">
+                <li id="relativeListItem2">Clare</li>
+                <li id="relativeListItem3">Edgar</li>
+                <li id="relativeListItem1">Shirley</li>
+                <li id="relativeListItem4">Wilson</li> 
+                <li id="relativeListItem5">Adrian</li>
+               </ol>
+
     </div>
   )
 }
